843642ca6e262a49aafc6a7df3af840483089387
Ummey Habiba Bristy <95018305+UmmeyHabibabristy@users.noreply.github.com>
2022-04-23 17:27:44 +0100

    FT2.php

---
+++ FINAL TASK 2/js/regi.js
@@ -0,0 +1,27 @@
+function login()
+{
+    var name = document.querySelector(".loginUname").value;
+    var email = document.querySelector(".loginEmail").value;
+    var pass = document.querySelector(".loginPass").value;
+    
+    if(name=="" || name.length<4)
+    {
+        document.getElementById("loginUnamep").innerHTML="Name must be greater than 3 characters";
+        return false;
+    }
+    else if(pass=="" || pass.length<7)
+    {
+        document.getElementById("loginPassp").innerHTML="Password must be greater than 6 character";
+        return false;
+    }
+    else if (email=="" || !email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/))
+    {
+        document.getElementById("loginEmailp").innerHTML="Not a valid Email";
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+© 2022 GitHub, Inc.
